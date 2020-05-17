1201228f7fe0508a39597f7a6970cb7e102ad805
alanhcrdz <alanhcrdz@gmail.com>
2020-05-17 19:11:48 -0300

    Inital commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cálculo do Índice de Massa Corporal (IMC).
+
+# Neste projeto, foi usado um formulário de contato, bem como comandos no   javascript para validar os campos.
+
+# User stories:
+
+# 1 - Usuário pode digitar o peso em kg. (não é necessário abreviação).
+# 2 - Usuário pode digitar a sua altura. (Somente é permitido "." como separador decimal).
+# 3 - Usuário pode digitar a idade, sendo este campo, opcional.
+# 4 - Usuário poderá escolher o sexo, também opcional.
+
+# Atualizações serão feitas ao longo do tempo
---
+++ script.js
@@ -0,0 +1,81 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    var weight = document.querySelector('#weight').value;
+    var height = document.querySelector('#height').value;
+    var age = document.querySelector('#quantity').value;
+
+    weight = parseFloat(weight);
+    height = parseFloat(height);
+    age = parseInt(age);
+
+
+    
+        calculateBMI(weight, height, age);
+    
+  
+
+
+  
+})
+
+
+function calculateBMI(wght, hght, age){
+var bmi = wght / (hght * hght);
+bmi = parseFloat(bmi).toFixed(2);
+
+var span = document.querySelector('#span');
+
+
+switch (true) {
+    case (bmi < 18.5) :
+        span.innerHTML = "Seu IMC é de: " + bmi + '. ' + 'Atenção! você está abaixo do peso adequado.'
+        span.style.color = 'hsl(33.33, 88.39%, 56.08%)';
+        break;
+
+        case (bmi >= 18.5  && bmi <= 24.9) :
+            span.innerHTML = "Seu IMC é de: " + bmi + '.' + ' Parabéns, você está dentro do peso adequado.'
+            span.style.color = 'hsl(165.17, 82.08%, 41.57%)';
+        break;
+
+        case (bmi >= 24.9  && bmi <= 30) :
+            span.innerHTML = "Seu IMC é de: " + bmi + '.' + ' Atenção, você está acima do peso adequado.'
+        span.style.color = 'hsl(33.33, 88.39%, 56.08%)';
+            break;
+
+            case (bmi > 30) :
+                span.innerHTML = "Seu IMC é de: " + bmi + '.' + ' Cuidado, você está muito acima do peso adequado.'
+                span.style.color = 'hsl(0, 100%, 55.29%)';  
+            break;
+
+    default:
+        break;
+}
+
+
+ //verify if has blank spaces
+/*  var regex = /^[0-9.,]$/;
+var weight_error = document.getElementById('weight-error');
+ 
+    if (!regex.test(wght))
+    {
+        weight_error.innerHTML = "Campo inválido. Preencha corretamente" ;
+        return false;
+    }else{
+        weight_error.style.display = "none" ;
+        age_error.style.display = "none" ;
+    }
+    return true;
+     */
+
+}/**/
+
+
+
+
+
+
+
+
